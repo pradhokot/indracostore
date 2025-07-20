5e37ea42ec64a4a1a20786ab3c4df901f3521825
pradhokot <yoggi60@gmail.com>
2025-07-20 20:56:24 +0700

    update 2

---
+++ templates/template1/layout/language.js
@@ -0,0 +1,12 @@
+document.writeln(`
+<a href="#" class="nav-link py-2 px-0 p-md-0" id="nav-en">
+   <span class="link-text">
+      <img src="../../assets/img/flag-eng.png" width="16" height="auto" alt=""> ENGLISH
+   </span>
+</a>
+<div class="vr h-100 d-none d-md-inline"></div>
+<a href="#" class="nav-link py-2 px-0 p-md-0" id="nav-id">
+   <span class="link-text">
+      <img src="../../assets/img/flag-ina.png" width="16" height="auto" alt=""> INDONESIA
+   </span>
+</a>`)
