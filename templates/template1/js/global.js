35a43e75c546fc5c21f22ab27edc0e9a9b8480f6
pradhokot <yoggi60@gmail.com>
2025-07-23 17:01:35 +0700

    update 4

--- templates/template1/js/global.js
+++ templates/template1/js/global.js
@@ -1,16 +1,22 @@
-// Fungsi untuk set top sidebar sesuai tinggi page-header
-function setSidebarTop() {
-   var header = document.querySelector('.page-header');
-   var sidebar = document.querySelector('.sidebar');
-   if (header && sidebar) {
-      var headerHeight = header.offsetHeight;
-      sidebar.style.top = headerHeight + 'px';
-   }
-   var columnSidebar = document.querySelector('.column-sidebar');
-   if (header && columnSidebar) {
-      var headerHeight = header.offsetHeight;
-      columnSidebar.style.top = headerHeight + 'px';
-   }
-}
-window.addEventListener('DOMContentLoaded', setSidebarTop);
-window.addEventListener('resize', setSidebarTop);
+$(document).ready(function () {
+
+   // set height column sidebar
+   var headerHeight = $('body > header').outerHeight();
+   $('.column-sidebar').css('top', headerHeight + 'px');
+
+   // layering when offcanvas active
+   $('#menu').on('shown.bs.offcanvas', function () {
+      $('body > header').addClass('z-3');
+   });
+   $('#menu').on('hidden.bs.offcanvas', function () {
+      $('body > header').removeClass('z-3');
+   });
+   
+   /* $('#sidebar').on('shown.bs.offcanvas', function () {
+      $('body > main').addClass('z-3');
+   });
+   $('#sidebar').on('hidden.bs.offcanvas', function () {
+      $('body > main').removeClass('z-3');
+   }); */
+
+})
