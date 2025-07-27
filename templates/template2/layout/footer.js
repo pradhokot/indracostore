783a3c4c3fde8319f08920717a6a54e78ce11a04
pradhokot <yoggi60@gmail.com>
2025-07-27 11:05:27 +0700

    update 8

---
+++ templates/template2/layout/footer.js
@@ -0,0 +1,74 @@
+document.writeln(`
+<footer aria-label="page footer" class="small py-3" style="background-color: #f1f2f2; padding-top: 2rem !important;">
+   <div class="container">
+      <div class="row" style="row-gap: 2rem;">
+         <div class="col col-12 col-lg-auto order-lg-2 d-flex flex-column column-sidefooter">
+            <div class="flex-grow-1">
+               <h5 class="fs-6 text-uppercase d-none">
+                  <strong class="fw-medium">sosial media</strong>
+               </h5>
+               <script src="layout/sosmed.js"></script>
+            </div>
+            <div class="mb-lg-3">
+               <p>
+                  Bergabunglah dengan kami dan dapatkan penawaran-penawaran menarik lainnya.
+               </p>
+               <a href="#" class="btn btn-outline-dark rounded-0 w-100 text-uppercase">
+                  subscribe
+               </a>
+            </div>
+         </div><!-- end col -->
+         <div class="col col-12 col-lg order-lg-1">
+            <div class="row row-cols-2 row-gap-3 row-cols-md-auto justify-content-md-between row-cols-lg-auto justify-content-lg-start g-lg-0 column-gap-lg-5">
+               <div class="col">
+                  <h5 class="fs-6 text-uppercase">
+                     <strong class="fw-medium">informasi</strong>
+                  </h5>
+                  <ul class="list-unstyled text-capitalize d-grid row-gap-2">
+                     <li><a href="#" class="text-reset">hubungi kami</a></li>
+                     <li><a href="#" class="text-reset">tentang kami</a></li>
+                     <li><a href="#" class="text-reset">berita</a></li>
+                     <li><a href="#" class="text-reset">promo</a></li>
+                     <li><a href="#" class="text-reset">KOL</a></li>
+                  </ul>
+               </div><!-- end col -->
+               <div class="col">
+                  <h5 class="fs-6 text-uppercase">
+                     <strong class="fw-medium">akun</strong>
+                  </h5>
+                  <ul class="list-unstyled text-capitalize d-grid row-gap-2">
+                     <li><a href="#" class="text-reset">akun saya</a></li>
+                     <li><a href="#" class="text-reset">konfirmasi pembayaran</a></li>
+                     <li><a href="#" class="text-reset">pesanan saya</a></li>
+                     <li><a href="#" class="text-reset">lacak pengiriman</a></li>
+                  </ul>
+               </div><!-- end col -->
+               <div class="col">
+                  <h5 class="fs-6 text-uppercase">
+                     <strong class="fw-medium">belanja</strong>
+                  </h5>
+                  <ul class="list-unstyled text-capitalize d-grid row-gap-2">
+                     <li><a href="#" class="text-reset">cara pembelian</a></li>
+                     <li><a href="#" class="text-reset">cara pembayaran</a></li>
+                     <li><a href="#" class="text-reset">cara pengembalian</a></li>
+                  </ul>
+               </div><!-- end col -->
+               <div class="col">
+                  <h5 class="fs-6 text-uppercase">
+                     <strong class="fw-medium">layanan</strong>
+                  </h5>
+                  <ul class="list-unstyled text-capitalize d-grid row-gap-2">
+                     <li><a href="#" class="text-reset">kebijakan privasi</a></li>
+                     <li><a href="#" class="text-reset">syarat & ketentuan</a></li>
+                     <li><a href="#" class="text-reset">FAQ</a></li>
+                  </ul>
+               </div><!-- end col -->
+            </div><!-- end row -->
+         </div><!-- end col -->
+      </div><!-- end row -->
+   </div><!-- end container -->
+   <hr>
+   <p class="text-center opacity-75">
+      &copy; 2025 INDRACOSTORE. All rights reserved
+   </p>
+</footer><!-- end page footer -->`)
--- templates/template2/layout/trigger-account.js
+++ templates/template2/layout/trigger-account.js
@@ -1,31 +1,37 @@
 document.writeln(`
 <div class="dropdown">
-   <button class="btn rounded-0 border-0 dropdown-toggle" data-bs-toggle="dropdown" data-bs-target="#dropdown-user">
+   <button class="btn rounded-0 border-0 dropdown-toggle pe-0">
       <i class="fas fa-user"></i><span class="visually-hidden">User</span>
    </button>
-   <ul class="dropdown-menu dropdown-menu-end rounded-0" id="dropdown-user">
+   <ul class="dropdown-menu dropdown-menu-end rounded-0 text-capitalize">
       <li>
          <a href="#" class="dropdown-item media">
             <span class="media-header"><i class="fas fa-user-plus"></i></span>
-            <span class="media-body">Sign Up</span>
+            <span class="media-body">akun saya</span>
          </a>
       </li>
       <li>
          <a href="#" class="dropdown-item media">
             <span class="media-header"><i class="fas fa-user"></i></span>
-            <span class="media-body">Sign In</span>
+            <span class="media-body">konfirmasi pembayaran</span>
          </a>
       </li>
       <li>
          <a href="#" class="dropdown-item media">
             <span class="media-header"><i class="fas fa-user-xmark"></i></span>
-            <span class="media-body">Sign Out</span>
+            <span class="media-body">pesanan saya</span>
          </a>
       </li>
       <li>
          <a href="#" class="dropdown-item media">
             <span class="media-header"><i class="fas fa-user"></i></span>
-            <span class="media-body">Payment Confirmation</span>
+            <span class="media-body">lacak pengiriman</span>
+         </a>
+      </li>
+      <li>
+         <a href="#" class="dropdown-item media">
+            <span class="media-header"><i class="fas fa-user"></i></span>
+            <span class="media-body">keluar</span>
          </a>
       </li>
    </ul>
